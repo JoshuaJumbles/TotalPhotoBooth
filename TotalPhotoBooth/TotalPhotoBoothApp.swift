931d30238a6700866fb39e57b939afb6afbefc0c
JoshuaJumbles <joshsaf@gmail.com>
2026-07-22 14:36:36 -0400

    Initial Commit

---
+++ TotalPhotoBooth/TotalPhotoBoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  TotalPhotoBoothApp.swift
+//  TotalPhotoBooth
+//
+//  Created by Joshua Jumbles on 7/22/26.
+//
+
+import SwiftUI
+
+@main
+struct TotalPhotoBoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
